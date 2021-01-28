71a7d57989b2e3e8976e63726c6757ee3f7ef46a
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-28 22:52:08 +0900

    first commit

---
+++ swift_tutorial_senni/NextViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NextViewController.swift
+//  swift_tutorial_senni
+//
+//  Created by 早川マイケル on 2021/01/28.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
