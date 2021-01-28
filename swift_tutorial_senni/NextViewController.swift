10192f979a1f01d4856108524f08a6003f1e14ff
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-28 23:54:46 +0900

    complete

--- swift_tutorial_senni/NextViewController.swift
+++ swift_tutorial_senni/NextViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    @IBOutlet weak var label2: UILabel!
+    
+    var count2 = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        label2.text = String(count2)
     }
     
 
--- swift_tutorial_senni/ViewController.swift
+++ swift_tutorial_senni/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = String(count)
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        count = count + 1
+        
+        label.text = String(count)
+        
+        if count == 10 {
+            
+            // 画面遷移をする
+ //           performSegue(withIdentifier: "next", sender: nil)
+   
+            let nextVC = storyboard?.instantiateViewController(withIdentifier: "next") as! NextViewController
+            navigationController?.pushViewController(nextVC, animated: true)
+            
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! NextViewController
+        nextVC.count2 = count
+    }
+    
 }
 
